6364e660d89fe94117920874715debdb7e8641d9
alex47spb <heap.styx@mail.ru>
2020-09-30 17:56:02 +0300

    feat: min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
+exports.min = function min(array) {
+    if (array === null) return 0;
+    if (typeof array == "undefined") return 0;
+    if (array.constructor !== Array) return 0;
+    if (array.length === 0) return 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    var result = array[0];
 
-exports.max = function max (array) {
-  return 0;
-}
+    for (var i = 1; i < array.length; i++) {
+        if (result > array[i]) result = array[i];
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return result;
+};
+
+exports.max = function max(array) {
+    if (array === null) return 0;
+    if (typeof array == "undefined") return 0;
+    if (array.constructor !== Array) return 0;
+    if (array.length === 0) return 0;
+
+    var result = array[0];
+
+    for (var i = 1; i < array.length; i++) {
+        if (result < array[i]) result = array[i];
+    }
+
+    return result;
+};
+
+exports.avg = function avg(array) {
+    if (array === null) return 0;
+    if (typeof array == "undefined") return 0;
+    if (array.constructor !== Array) return 0;
+    if (array.length === 0) return 0;
+
+    var result = 0;
+
+    for (var i = 0; i < array.length; i++) {
+        result += array[i];
+    }
+
+    result = result / array.length;
+
+    return result;
+};
